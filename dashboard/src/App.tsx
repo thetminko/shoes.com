705e9a995925f662fa676554f6c7e2cc7c76431c
Thet Min Ko <dev.minko@gmail.com>
2021-04-09 14:39:18 +0800

    Clean up

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact={true}>
-            {/* home page */}
-          </Route>
-          <Route path="/checkout" exact={true}>
-            {/* After checkout page */}
-          </Route>
-          <Route path="/history" exact={true}>
-            {/* Order history page */}
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
-}
+export const App: FC = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true}>
+          {/* home page */}
+        </Route>
+        <Route path="/checkout" exact={true}>
+          {/* After checkout page */}
+        </Route>
+        <Route path="/history" exact={true}>
+          {/* Order history page */}
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
