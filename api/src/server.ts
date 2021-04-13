9132464a2ded87f00c15e99ea9d4668403fe03b4
Thet Min Ko <dev.minko@gmail.com>
2021-04-13 18:35:52 +0800

    Allow cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,14 @@
+import cors from 'cors';
 import express, { Application } from 'express';
 import { apiController } from './controllers';
 import { initSequelize } from './sequelize';
 
 // Create a new express application instance
 const app: Application = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(apiController);
 
